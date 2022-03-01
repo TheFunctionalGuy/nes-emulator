d5f4af763892d2978d519eca78d93ec2396fe3b1
Julian V <jvesper95@gmail.com>
2022-03-02 00:59:30 +0100

    Refactor code by moving TAX and LDA instruction into function
    Move updating zero and negative flag to own function also

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,30 @@ impl CPU {
         }
     }
 
+    fn lda(&mut self, value: u8) {
+        self.register_a = value;
+        self.update_zero_and_negative_flags(self.register_a);
+    }
+
+    fn tax(&mut self) {
+        self.register_x = self.register_a;
+        self.update_zero_and_negative_flags(self.register_x);
+    }
+
+    fn update_zero_and_negative_flags(&mut self, result: u8) {
+        if result == 0 {
+            self.status |= ZERO_FLAG;
+        } else {
+            self.status &= ZERO_FLAG_INV;
+        }
+
+        if result & NEGATIVE_FLAG != 0 {
+            self.status |= NEGATIVE_FLAG;
+        } else {
+            self.status &= NEGATIVE_FLAG_INV;
+        }
+    }
+
     pub fn interpret(&mut self, program: Vec<u8>) {
         self.program_counter = 0;
 
@@ -35,36 +59,11 @@ impl CPU {
                 0xA9 => {
                     let param = program[self.program_counter as usize];
                     self.program_counter += 1;
-                    self.register_a = param;
 
-                    if self.register_a == 0 {
-                        self.status |= ZERO_FLAG;
-                    } else {
-                        self.status &= ZERO_FLAG_INV;
-                    }
-
-                    if self.register_a & NEGATIVE_FLAG != 0 {
-                        self.status |= NEGATIVE_FLAG;
-                    } else {
-                        self.status &= NEGATIVE_FLAG_INV;
-                    }
+                    self.lda(param);
                 }
                 // TAX (0xAA)
-                0xAA => {
-                    self.register_x = self.register_a;
-
-                    if self.register_x == 0 {
-                        self.status |= ZERO_FLAG;
-                    } else {
-                        self.status &= ZERO_FLAG_INV;
-                    }
-
-                    if self.register_x & NEGATIVE_FLAG != 0 {
-                        self.status |= NEGATIVE_FLAG;
-                    } else {
-                        self.status &= NEGATIVE_FLAG_INV;
-                    }
-                }
+                0xAA => self.tax(),
                 // BRK (0x00)
                 0x00 => {
                     return;
