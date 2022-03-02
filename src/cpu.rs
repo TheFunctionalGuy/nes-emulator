4d139dfb8ad1ce2b2e3ce5d6d65f0815ab6e3d87
Julian V <jvesper95@gmail.com>
2022-03-02 13:55:26 +0100

    Add little-endian memory functions for 16-bit values

--- src/cpu.rs
+++ src/cpu.rs
@@ -31,8 +31,25 @@ impl CPU {
 		self.memory[addr as usize]
 	}
 
+	// TODO: Refactor to use from_le_bytes()
+	fn mem_read_u16(&mut self, addr: u16) -> u16 {
+		let lo = self.mem_read(addr) as u16;
+		let hi = self.mem_read(addr + 1) as u16;
+
+		(hi << 8) | lo
+	}
+
 	fn mem_write(&mut self, addr: u16, data: u8) {
 		self.memory[addr as usize] = data;
+	}
+
+	// TODO: Refactor to use to_le_bytes()
+	fn mem_write_u16(&mut self, addr: u16, data: u16) {
+		let hi = (data >> 8) as u8;
+		let lo = (data & 0xFF) as u8;
+
+		self.mem_write(addr, lo);
+		self.mem_write(addr + 1, hi);
 	}
 
 	// CPU operations
