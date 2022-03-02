4dae9432f8cb74d2b6d49f620573c218761f90c1
Julian V <jvesper95@gmail.com>
2022-03-02 15:27:28 +0100

    Rename test variables

--- src/cpu.rs
+++ src/cpu.rs
@@ -226,8 +226,8 @@ mod test {
 	fn test_0xa9_lda_immediate_load_data() {
 		let mut cpu = CPU::new();
 
-		let test_binary = vec![0xA9, 0x05, 0x00];
-		cpu.load_and_run(test_binary);
+		let binary = vec![0xA9, 0x05, 0x00];
+		cpu.load_and_run(binary);
 
 		assert_eq!(cpu.register_a, 0x05);
 		// Z flag unset
@@ -241,8 +241,8 @@ mod test {
 		let mut cpu = CPU::new();
 		cpu.mem_write(0x10, 0x55);
 
-		let test_binary = vec![0xA5, 0x10, 0x00];
-		cpu.load_and_run(test_binary);
+		let binary = vec![0xA5, 0x10, 0x00];
+		cpu.load_and_run(binary);
 
 		assert_eq!(cpu.register_a, 0x55);
 	}
@@ -251,8 +251,8 @@ mod test {
 	fn test_0xa9_lda_zero_flag() {
 		let mut cpu = CPU::new();
 
-		let test_binary = vec![0xA9, 0x00, 0x00];
-		cpu.load_and_run(test_binary);
+		let binary = vec![0xA9, 0x00, 0x00];
+		cpu.load_and_run(binary);
 
 		// Z flag set
 		assert!(cpu.status & 0b0000_0010 == 0b10);
@@ -262,8 +262,8 @@ mod test {
 	fn test_0xaa_tax_move_a_to_x() {
 		let mut cpu = CPU::new();
 
-		let test_binary = vec![0xA9, 0x0A, 0xAA, 0x00];
-		cpu.load_and_run(test_binary);
+		let binary = vec![0xA9, 0x0A, 0xAA, 0x00];
+		cpu.load_and_run(binary);
 
 		assert_eq!(cpu.register_x, 10);
 	}
@@ -273,8 +273,8 @@ mod test {
 		let mut cpu = CPU::new();
 		cpu.mem_write(0x0102, 0xFF);
 
-		let test_binary = vec![0xAD, 0x02, 0x01, 0x00];
-		cpu.load_and_run(test_binary);
+		let binary = vec![0xAD, 0x02, 0x01, 0x00];
+		cpu.load_and_run(binary);
 
 		assert_eq!(cpu.register_a, 0xFF);
 	}
@@ -283,8 +283,8 @@ mod test {
 	fn test_inx_overflow() {
 		let mut cpu = CPU::new();
 
-		let test_binary = vec![0xA9, 0xFF, 0xAA, 0xE8, 0xE8, 0x00];
-		cpu.load_and_run(test_binary);
+		let binary = vec![0xA9, 0xFF, 0xAA, 0xE8, 0xE8, 0x00];
+		cpu.load_and_run(binary);
 
 		assert_eq!(cpu.register_x, 1);
 	}
@@ -293,8 +293,8 @@ mod test {
 	fn test_5_ops_working_together() {
 		let mut cpu = CPU::new();
 
-		let test_binary = vec![0xA9, 0xC0, 0xAA, 0xE8, 0x00];
-		cpu.load_and_run(test_binary);
+		let binary = vec![0xA9, 0xC0, 0xAA, 0xE8, 0x00];
+		cpu.load_and_run(binary);
 
 		assert_eq!(cpu.register_x, 0xC1);
 	}
